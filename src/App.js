21ffb443bef79ce924dc49a59e2157c286ec3557
2017244 <72963919+2017244@users.noreply.github.com>
2020-11-06 11:15:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing changes!
+          changes in class!
         </p>
         <a
           className="App-link"
